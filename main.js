3790f44c998246853903841fba94b909da9804b5
Nikhil Gaikwad <gaikwadrnikhil@gmail.com>
2020-05-16 15:56:56 -0500

    fixed all the changes and finished the open sale ticket

--- main.js
+++ main.js
@@ -439,21 +439,24 @@ const addImageToAboutNate=()=>{
     img.src = 'https://previews.123rf.com/images/elnur/elnur1311/elnur131100481/23538761-funny-man-with-trousers-isolated-on-white.jpg';
 }
 
-const buildDomString=()=>{
+const buildDomString=(randomNumber)=>{
     let saleString="";
     saleString += 
-    `<h5> SALE <span class="red">SALE</span> SALE <span class="red">SALE</span> SALE<span class="red">SALE</span> SALE </h5>
-    <h2 class="big display-4">BIG<h1>
-    <h2 class="sale display-4">SALE<h1>
-    <p class="site"> ONLINE ONLY | <span class="entireSection">20% OFF</span></p>
-    <P class="shopNow">$ ${calculateDiscount(pantsArray[10].price)}</p>
+    `<div class="saleText">
+    <h2 class="big">BIG<h1>
+    <h2 class="sale">SALE<h1>
+    <p> <span class="entireSection">20% OFF</span></p>
+    <P class="shopNow"> ${pantsArray[randomNumber].model}</p>
+    <P class="original">Price: ${pantsArray[randomNumber].price}</p>
+    <P class="shopNow">$ ${calculateDiscount(pantsArray[randomNumber].price)}</p>
+    </div>
     `;
     return saleString;
 }
 
-const saleDetails=()=>{
+const saleDetails=(randomNumber)=>{
     let model= document.querySelector(".saleDetails");
-    model.innerHTML= buildDomString();
+    model.innerHTML= buildDomString(randomNumber);
 
 }
 
@@ -465,8 +468,14 @@ return (price * .8).toFixed(2);
 
 const addSaleImg=()=>{
     let img = document.querySelector(".saleImg");
-    img.src = images[10];
-    saleDetails();
+    randomNumber = pickRandomNumber(pantsArray.length)
+    img.src = images[randomNumber];
+    saleDetails(randomNumber);
+
+}
+
+const pickRandomNumber=(num)=>{
+  return Math.floor(Math.random() * Math.floor(num));
 
 }
 
